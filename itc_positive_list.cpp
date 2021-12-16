cece076fc7b3bd696cb2c845067dfeeabe5f494d
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-12-16 20:16:18 +0300

    Update itc_positive_list.cpp

--- itc_positive_list.cpp
+++ itc_positive_list.cpp
@@ -1,8 +1,8 @@
 #include "easy_list.h"
 
 int itc_positive_list(const vector <int> &mass) {
+    int counter = 0;
     if (mass.size() != 0){
-        int counter = 0;
         for (int i = 0; i < mass.size(); i++)
             if (mass[i] >= 0)
                 counter += 1;
